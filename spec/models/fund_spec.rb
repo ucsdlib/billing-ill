279dc39ee587571206be01a66d8e1e94a102a6cb
Huawei Weng <hweng@ucsd.edu>
2015-01-29 14:10:23 -0800

    config shoulda-matchers(IB-17,IB-18)

---
+++ spec/models/fund_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Fund do 
+
+  it {should have_many(:recharges)}
+  
+  it {should validate_presence_of(:program_code)}
+  it {should validate_presence_of(:org_code)}
+  it {should validate_presence_of(:index_code)}
+  it {should validate_presence_of(:fund_code)}
+
+  it {should validates_length_of(:program_code)}
+  it {should validates_length_of(:org_code)}
+  it {should validates_length_of(:index_code)}
+  it {should validates_length_of(:fund_code)}
+end
