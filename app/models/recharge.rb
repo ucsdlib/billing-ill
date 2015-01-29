db6bb9a9c10fdfee196ff7c95b3c7bc862ef011b
Huawei Weng <hweng@ucsd.edu>
2015-01-29 12:04:28 -0800

    Fixing typo in recharge model.(IB-17)

--- app/models/recharge.rb
+++ app/models/recharge.rb
@@ -2,7 +2,7 @@ class Recharge < ActiveRecord::Base
   belongs_to :fund
 
   validates :charge, presence: true, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0, :less_than => 1000000}
-  validates :number_copies, presence: true, 
-  validates :status, presence: true, 
+  validates :number_copies, presence: true
+  validates :status, presence: true 
   
 end
