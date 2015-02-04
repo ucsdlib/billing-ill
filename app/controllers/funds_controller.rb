16322a11e191fbb5de98aae4ca8d82f28ce1268d
Huawei Weng <hweng@ucsd.edu>
2015-02-04 12:45:18 -0800

    create page controller and adding front page.(IB-22)

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -3,4 +3,20 @@ class FundsController < ApplicationController
   def new
     @fund = Fund.new
   end
+
+  def create
+    @fund = Fund.new(fund_params)
+
+    if @fund.save
+      redirect_to home_path, notice: 'A new fund is created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def fund_params
+    params.require(:fund).permit(:program_code, :org_code, :index_code, :fund_code)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+ root :to => 'page#front'
+  
  get 'ui(/:action)', controller: 'ui'
 
  get '/ruby-version' => 'application#ruby_version'
