2dacd60400fb7ecb5d71018a0c7624cd8f73a730
Huawei Weng <hweng@ucsd.edu>
2015-02-05 11:26:27 -0800

    implementing the edit form for Fund.(IB-24)

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -1,4 +1,5 @@
 class FundsController < ApplicationController
+  before_action :set_fund, only: [:edit, :update]
 
   def new
     @fund = Fund.new
@@ -8,9 +9,22 @@ class FundsController < ApplicationController
     @fund = Fund.new(fund_params)
 
     if @fund.save
-      redirect_to root_path, success: 'A new fund is created!'
+      redirect_to root_path, notice: 'A new fund is created!'
     else
       render :new
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @fund.update(fund_params)
+      flash[:notice] = "Your fund was updated"
+      redirect_to root_path
+    else
+      render :edit
     end
   end
 
@@ -18,5 +32,9 @@ class FundsController < ApplicationController
 
   def fund_params
     params.require(:fund).permit(:program_code, :org_code, :index_code, :fund_code)
+  end
+
+  def set_fund
+    @fund = Fund.find_by params[:id]
   end
 end
