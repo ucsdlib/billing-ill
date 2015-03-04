9d0c5d53c4a7aaac7822c5bba25757c79d465b22
Huawei Weng <hweng@ucsd.edu>
2015-03-04 11:17:20 -0800

    adding description column to funds table.(IB-36)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129222653) do
+ActiveRecord::Schema.define(version: 20150304191115) do
 
   create_table "funds", force: :cascade do |t|
     t.string   "program_code"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150129222653) do
     t.string   "fund_code"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "recharges", force: :cascade do |t|
