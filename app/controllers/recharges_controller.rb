5f597eca942eafdc52f653047c5e178eaecf2706
Huawei Weng <hweng@ucsd.edu>
2015-03-09 15:35:15 -0700

    Fixing sequence number format convert. (IB-39)

--- app/controllers/recharges_controller.rb
+++ app/controllers/recharges_controller.rb
@@ -66,14 +66,14 @@ class RechargesController < ApplicationController
     # detail_rows
     d_column1_19 = "LIBRARY1" + "01" + "FRLBG551" + "2"
     d_column24_27 = "F510"
-    d_column40_76 = "LIBRARY-PHOTOCOPY SERVICE" + "D" + "A"
+    d_column40_76 = "LIBRARY-PHOTOCOPY SERVICE" + " " * 10 + "D" + "A"
     d_column89_94 = "636064"
     d_column101_112 = " " * 6 + " " * 6
     d_column123_209 = " " * 32 + "000000" + " " * 17 + "000000" + " " * 10 + "0000" + "0000" + " " * 9
     d_column220 = " "
 
     detail_rows = ""
-    total_amount = 0
+    total_charge = 0
     result_arr.each_with_index do |recharge, index|
       sequence_num = convert_seq_num(index)
       charge = recharge.charge
@@ -83,18 +83,18 @@ class RechargesController < ApplicationController
       program_code = recharge.fund_program_code
       index_code = recharge.fund_index_code
       filler_var = recharge.created_at.strftime("%Y%m%d") + " " * 2
-      total_amount += charge
+      total_charge += charge
 
-      detail_rows += "#{column1_19}#{sequence_num}#{d_column24_27}#{transaction_amount}#{d_column40_76}#{fund_code}#{org_code}"
+      detail_rows += "#{d_column1_19}#{sequence_num}#{d_column24_27}#{transaction_amount}#{d_column40_76}#{fund_code}#{org_code}"
       detail_rows += "#{d_column89_94}#{program_code}#{d_column101_112}#{index_code}#{d_column123_209}#{filler_var}#{d_column220}\n"
     end
-
+    
     #final credit row
     f_column1_19 = "LIBRARY1" + "01" + "FRLBG551" + "2"
     f_sequence_num = convert_seq_num(result_arr.size + 1)
     f_column24_27 = "F510"
-    total_amount = total_amount.convert_charge(total_amount)
-    f_column40_76 = "LIBRARY-PHOTOCOPY SERVICE" + "C" + "A"
+    total_amount = convert_charge(total_charge)
+    f_column40_76 = "LIBRARY-PHOTOCOPY SERVICE" + " " * 10 + "C" + "A"
     f_column77_112 = " " * 12 + "693900" + " " * 18
     f_column113_122 = "LIBIL05" + " " * 3
     f_column123_154 = " " * 32
@@ -102,7 +102,7 @@ class RechargesController < ApplicationController
     f_filler_var = " " * 10 #to fix
     f_column220 = " "
 
-    document_amount = convert_charge(total_amount * 2)
+    document_amount = convert_charge(total_charge * 2)
 
     header_row = "#{h_column1_19}#{h_column20_54}#{transaction_date}#{document_amount}#{h_column75_250}\n"
     
@@ -115,19 +115,8 @@ class RechargesController < ApplicationController
 
   private
 
-   def convert_seq_num (seq_num)
-    n = 0
-    if seq_num < 10
-      n = 3
-    elsif seq_num >=10 && seq_num < 100  
-      n = 2
-    elsif seq_num >=100 && seq_num < 1000
-      n = 1
-    else
-      n = 0
-    end
-      
-    str = seq_num.to_s.rjust(n, "0")
+  def convert_seq_num (seq_num)
+    str = seq_num.to_s.rjust(4, "0") # 1 --> 0001, 10 --> 0010
   end
 
   def convert_charge(amount)
