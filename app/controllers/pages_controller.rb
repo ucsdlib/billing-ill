c2ecd81a16ff272cb9d419062197f03953142cc9
Huawei Weng <hweng@ucsd.edu>
2015-03-23 16:13:15 -0700

    Fixing flash message.(IB-28)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 
   def front
-    flash[:alert] = ('You have been logged out of ILL BILLING. To logout of all Single Sign-On applications, close your browser or <a href="/Shibboleth.sso/Logout?return=https://a4.ucsd.edu/tritON/logout?target='+root_url+'">terminate your Shibboleth session</a>.').html_safe 
+    
   end
 end
