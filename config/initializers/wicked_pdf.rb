27b45b27800dd9f48c881e614e8ed7b95257c5ec
Huawei Weng <hweng@ucsd.edu>
2015-05-21 11:09:03 -0700

    Updating wicked_pdf config.(IB-71)

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,7 @@
-WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  exe_path: '/usr/local/bin/wkhtmltopdf'
-}
+if Rails.env.development?
+  WickedPdf.config = {
+    #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
+    #:layout => "pdf.html",
+    exe_path: '/usr/local/bin/wkhtmltopdf'
+  }
+end
