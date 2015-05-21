27bea2dad3ee87aaaa08636e8b78abe4d32a75b5
Huawei Weng <hweng@ucsd.edu>
2015-05-21 13:57:08 -0700

    Adding merge records function.(IB-77)

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
      get 'create_entity_output', to: 'invoices#create_entity_output'
      get 'ftp_file', to: 'invoices#ftp_file'
      get 'create_report', to: 'invoices#create_report'
+     get 'merge_records', to: 'invoices#merge_records'
    end
  end
 
