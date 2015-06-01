ec43a9bfd8440efc331bda8732b01714eb13672f
Huawei Weng <hweng@ucsd.edu>
2015-06-01 10:30:08 -0700

    Handling assest pipeline issues for pdf generation on staging and prod.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -19,6 +19,8 @@ Rails.application.configure do
    # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  config.assets.precompile += ['invoice.sass']
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
