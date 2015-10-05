3ed24589165fb3072f9261c395ec1bb12ec9a04d
Huawei Weng <hweng@ucsd.edu>
2015-10-05 15:53:58 -0700

    test access control updates.

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,39 +59,64 @@ class User < ActiveRecord::Base
   #   return result
   # end
 
- def self.lookup_group(uid)
-   result_user = groups_for_user(uid)
- end
+#  def self.lookup_group(uid)
+#    result_user = groups_for_user(uid)
+#  end
 
- def self.groups_for_user(username, group_attribute='cn')
-  dn = "CN=#{username},#{Hydra::LDAP.ldap_config[:base]}"
-  filter=Net::LDAP::Filter.construct("(&(objectCategory=group)(member=#{dn}))")
-  results = Hydra::LDAP.groups_for_user(filter,group_attribute) { |result|
-    result.map { |r| r[group_attribute].first }
-  }
-  puts "results are: " + results
-end
+#  def self.groups_for_user(username, group_attribute='cn')
+#   dn = "CN=#{username},#{Hydra::LDAP.ldap_config[:base]}"
+#   filter=Net::LDAP::Filter.construct("(&(objectCategory=group)(member=#{dn}))")
+#   results = Hydra::LDAP.groups_for_user(filter,group_attribute) { |result|
+#     result.map { |r| r[group_attribute].first }
+#   }
+#   puts "results are: " + results
+# end
   
-  # def self.lookup_group(search_param)
+  def self.lookup_group(search_param)
     
-  #   result = ""
+    result = ""
 
-  #   ldap = get_ldap_connection
+    ldap = connection
 
-  #   result_attrs = ["sAMAccountName"]
-  #   search_filter = Net::LDAP::Filter.eq("sAMAccountName", search_param)
-  #   category_filter = Net::LDAP::Filter.eq("objectcategory", "user")
-  #   member_filter = Net::LDAP::Filter.eq("memberof", "CN=ILL Billing,OU=Groups,OU=University Library,DC=AD,DC=UCSD,DC=EDU")
-  #   s_c_filter = Net::LDAP::Filter.join(search_filter, category_filter)
-  #   composite_filter = Net::LDAP::Filter.join(s_c_filter, member_filter)
+    result_attrs = ["sAMAccountName"]
+    search_filter = Net::LDAP::Filter.eq("sAMAccountName", search_param)
+    category_filter = Net::LDAP::Filter.eq("objectcategory", "user")
+    member_filter = Net::LDAP::Filter.eq("memberof", "CN=ILL Billing,OU=Groups,OU=University Library,DC=AD,DC=UCSD,DC=EDU")
+    s_c_filter = Net::LDAP::Filter.join(search_filter, category_filter)
+    composite_filter = Net::LDAP::Filter.join(s_c_filter, member_filter)
 
-  #   ldap.search(:filter => composite_filter, :attributes => result_attrs, :return_result => false) { |item| 
-  #      result = item.sAMAccountName.first}
+    ldap.search(:filter => composite_filter, :attributes => result_attrs, :return_result => false) { |item| 
+       result = item.sAMAccountName.first}
 
-  #   get_ldap_response(ldap)
+    get_ldap_response(ldap)
 
-  #   return result
-  # end
+    return result
+  end
+
+  def self.connection
+      @ldap_conn ||= Net::LDAP.new(ldap_connection_config)
+  end
+
+  def self.ldap_connection_config
+    return @ldap_connection_config if @ldap_connection_config
+    @ldap_connection_config = {}
+    yml = ldap_config
+    @ldap_connection_config[:host] = yml[:host]
+    @ldap_connection_config[:port] = yml[:port]
+    if yml[:username] && yml[:password]
+      @ldap_connection_config[:auth]={:method=>:simple}
+      @ldap_connection_config[:auth][:username] = yml[:username]
+      @ldap_connection_config[:auth][:password] = yml[:password]
+      @ldap_connection_config[:base] = yml[:base]
+    end
+    @ldap_connection_config
+  end
+
+  def self.ldap_config
+    root = Rails.root || '.'
+    env = Rails.env || 'test'
+    @ldap_config ||= YAML::load(ERB.new(IO.read(File.join(root, 'config', 'hydra-ldap.yml'))).result)[env].with_indifferent_access
+  end
 
   def self.get_ldap_connection
     ldap = Net::LDAP.new  :host => Rails.application.secrets.ldap_host, 
