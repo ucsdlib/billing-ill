d27ea8913a32cd44bd1b3e526304998bbe3b4e26
Huawei Weng <hweng@ucsd.edu>
2015-12-03 15:19:23 -0800

    Updated ftp file naming convention for invoice.(IB-128)

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -19,9 +19,9 @@ class AppMailer < ActionMailer::Base
     @charge_count = Invoice.search_all_pending_status.size + 2
     @entity_count = Invoice.entity_count + 2
     @person_count = Invoice.person_count + 2
-    @charge_file = Invoice.create_charge_file
-    @entity_file = Invoice.create_entity_file
-    @person_file = Invoice.create_person_file
+    @charge_file = Invoice.charge_file_name
+    @entity_file = Invoice.entity_file_name
+    @person_file = Invoice.person_file_name
     @lcharge_file = Invoice.charge_lfile_name
     @lentity_file = Invoice.entity_lfile_name
     @lperson_file = Invoice.person_lfile_name
