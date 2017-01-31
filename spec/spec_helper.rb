205dbbd4720b275acdc59fc806ee2fccee7c9bf1
Huawei Weng <hweng@ucsd.edu>
2017-01-31 15:03:10 -0800

    Adding coveralls, hound and remove codeclimate.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
+
+require 'coveralls'
+Coveralls.wear!('rails')
 
 require 'rspec/matchers'
 #require 'equivalent-xml'
